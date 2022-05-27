865e3e2b4687487c08e83e4752c1a2853aeecde8
Azamat Ilyasov <Azamat3.1415@gmail.com>
2022-05-27 21:46:13 +0600

    updated_wacs_new - добавил измерения. и модель обновлена.

--- runFromExcel.py
+++ runFromExcel.py
@@ -31,7 +31,8 @@ LOGGING = {
 logging.config.dictConfig(LOGGING)
 logging.getLogger(__name__).debug('This is a debug message')
 
-net = pp.from_excel(os.path.join("C:", "\FILES", "PROJECTS", "pandafp", "demo", "EMS", "net_cspa2.xlsx"), convert=False)
+#net = pp.from_excel(os.path.join("C:", "\FILES", "PROJECTS", "pandafp", "demo", "EMS", "net_cspa2.xlsx"), convert=False)
+net = pp.from_excel(os.path.join("C:", "\FILES", "PROJECTS", "pandafp", "demo", "WACS", "wacs_new1.xlsx"), convert=False)
 
 print("NET:\n", net)
 print("MEAS.:\n", net.measurement)
@@ -54,7 +55,6 @@ try:
     #pp.runpp(net, max_iteration=10,  numba=False) 
     print("isRunPP: ", res_pp)  
     res_chi2 = est.chi2_analysis(net, init="flat")
-    
     print("isChi2: ", res_chi2)
     res_rn_max = est.remove_bad_data(net, init="flat", rn_max_threshold=3.0)
     print("isRemovedBadData: ", res_rn_max)
@@ -63,7 +63,8 @@ try:
 except Exception as e:
     print("ОШИБКА: " + str(e))
 
-pp.to_excel(net, os.path.join("C:", "\FILES", "PROJECTS", "pandafp", "demo", "EMS","net_cspa_result2.xlsx"))
+#pp.to_excel(net, os.path.join("C:", "\FILES", "PROJECTS", "pandafp", "demo", "EMS","net_cspa_result2.xlsx"))
+pp.to_excel(net, os.path.join("C:", "\FILES", "PROJECTS", "pandafp", "demo", "WACS","wacs_new1_result.xlsx"))
 
 
 # print("NET_BUS:\n", net.bus)
